de005a93a0e955cd45c625b0d45aac831fca8ce1
xxc <xxc13720385755@163.com>
2025-01-20 12:19:03 +0000

    rename to synsense

---
+++ synsense/preprocess/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This file contains all possible functions and orgnize them in
+a single pipeline so that preprocessing will be easier.
+"""
+
+class PipelineStream:
+    def __init__(self):
+        pass
+
+    
